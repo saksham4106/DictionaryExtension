15e6bd3a0bb5d543bf29d65a27d5deefaf152560
saksham4106 <saksham.vvip@gmail.com>
2024-06-04 20:15:53 +0530

    Add extension customization options

--- service/background.js
+++ service/background.js
@@ -1,4 +1,5 @@
 chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({"dark_mode": false})
     chrome.storage.sync.set({"max_meanings": 2})
+    chrome.storage.sync.set({"click_to_blur": false})
 })
--- views/js/popup.js
+++ views/js/popup.js
@@ -1,11 +1,18 @@
 window.onload = function () {
-    chrome.storage.sync.get(["dark_mode"], (result) => {
-        document.getElementById("flexSwitchCheckDefault").checked = result.dark_mode
+    chrome.storage.sync.get(["dark_mode"], (res1) => {
+        chrome.storage.sync.get(["max_meanings"], (res2) => {
+                chrome.storage.sync.get(["click_to_blur"], (res3) => {
+                    document.getElementById("dark_mode").checked = res1.dark_mode
+                    document.getElementById("max_meanings").value = res2.max_meanings
+                    document.getElementById("blur").checked = res3.click_to_blur
+            })
+        })
     })
 
-    document.getElementById("flexSwitchCheckDefault").addEventListener('click', () => {
-        var check = document.getElementById("flexSwitchCheckDefault")
-        chrome.storage.sync.set({"dark_mode": check.checked})
+    document.getElementById("submit").addEventListener('click', () => {
+        chrome.storage.sync.set({"dark_mode": document.getElementById("dark_mode").checked})
+        chrome.storage.sync.set({"max_meanings": Number(document.getElementById("max_meanings").value)})
+        chrome.storage.sync.set({"blur": document.getElementById("blur").checked})
     })
 }
 
